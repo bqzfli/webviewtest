8fda8b6ce76ebb984c228eb9f977e21042e9d58b
lisa <997745590@qq.com>
2017-06-27 23:02:11 +0800

    original

---
+++ webviewtest/src/main/assets/test.js
@@ -0,0 +1,10 @@
+
+function javacalljs(){
+     document.getElementById("content").innerHTML =
+         "<br\>JAVA调用了JS的无参函数";
+}
+
+function javacalljswith(arg){
+     document.getElementById("content").innerHTML =
+         ("<br\>"+arg);
+}
